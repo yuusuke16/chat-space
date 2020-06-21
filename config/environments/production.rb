fb8076336519ef30177d49546a632d56e78b6815
yuusuke16 <murakai1016@gmailcom>
2020-06-22 08:02:04 +0900

    自動デプロイ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  server '18.180.21.173', user: 'ec2-user', roles: %w{app db web}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
