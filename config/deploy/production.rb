b2cb45d83662f21390a33af84eb15869af4866a5
yuusuke16 <murakai1016@gmailcom>
2020-06-22 13:07:12 +0900

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '18.180.21.173', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
